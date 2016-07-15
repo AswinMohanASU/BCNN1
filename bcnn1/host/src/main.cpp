4430d0e307e511267991f27c9a93d52f83df741f
Aswin_Mohan <getaswin@gmail.com>
2016-07-15 12:59:22 -0700

    Conv Layer 1 - New attempt - Debugging kernelb1

--- bcnn1/host/src/main.cpp
+++ bcnn1/host/src/main.cpp
@@ -163,7 +163,7 @@ void run(){
 
     printf("Complete setting arguments \n");
 
-    global = {1, 1, 34};
+    global = {1, 1, 32};
     local = {1,1,1};
     err = clEnqueueNDRangeKernel(queue[0], kernel[0], 3, NULL, global, local, 0, NULL, NULL);
     checkerror(err,"Error: Failed to execute kernel[0]");
@@ -182,7 +182,7 @@ void run(){
 
     for(unsigned char i = 0; i < 1; i++){
         for(unsigned char j = 0; j < 1; j++){
-            for(unsigned char k = 0; k < 5; k++){
+            for(unsigned char k = 0; k < 32; k++){
                 count++;
                 //printf("Index %d ->> Expected = %d  Optained = %d\n",(k + (j * 32) + (i * (32*32))),w1[i][2][2][2], h_w1[ 2 + (2 * 3) + (2 * 3 * 3) + (i * 3 * 3 * 3)]);
 
