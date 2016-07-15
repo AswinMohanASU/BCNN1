3d2fe9dbb91de917f8ae53fe75556d0f0919dfc3
Aswin_Mohan <getaswin@gmail.com>
2016-07-15 15:34:47 -0700

    Conv Layer 1 - New attempt -  bcnn1/kernel2 - 1

--- bcnn1/host/src/main.cpp
+++ bcnn1/host/src/main.cpp
@@ -169,7 +169,7 @@ void run(){
     checkerror(err,"Error: Failed to set kernel arguments! - kernel[0] - d_debug");
     printf("Complete setting arguments \n");
 
-    global = {20, 32, 32};
+    global = {1, 32, 32};
     local = {1,32,32};
     err = clEnqueueNDRangeKernel(queue[0], kernel[0], 3, NULL, global, local, 0, NULL, NULL);
     checkerror(err,"Error: Failed to execute kernel[0]");
@@ -186,7 +186,7 @@ void run(){
     int count=0;
     int flag=0;
 
-    for(unsigned char i = 0; i < 20; i++){
+    for(unsigned char i = 0; i < 1; i++){
         for(unsigned char j = 0; j < 32; j++){
             for(unsigned char k = 0; k < 32; k++){
                 count++;
