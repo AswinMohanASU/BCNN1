ba2fc780eaf15673d1a036a703b6f9312bc66020
Aswin_Mohan <getaswin@gmail.com>
2016-07-16 23:41:07 -0700

    Conv Layer 1 - Update Kernel1  8 X 2kernels - offset - bug 3

--- bcnn1/host/src/main.cpp
+++ bcnn1/host/src/main.cpp
@@ -220,12 +220,12 @@ void run(){
     err = clEnqueueNDRangeKernel(queue[0], kernel[0], 3, NULL, global, NULL, 0, NULL, NULL);
     checkerror(err,"Error: Failed to execute kernel[0]");
 
-    err = clEnqueueNDRangeKernel(queue[0], kernel[1], 3, NULL, global, NULL, 0, NULL, NULL);
+    err = clEnqueueNDRangeKernel(queue[1], kernel[1], 3, NULL, global, NULL, 0, NULL, NULL);
     checkerror(err,"Error: Failed to execute kernel[1]");
 
     clFinish(queue[0]);
-  //  clFinish(queue[1]);
-    err = clEnqueueReadBuffer( queue[0] , d_act1, CL_TRUE, 0, sizeof(int) * 128 * 32 * 32, &h_act1, 0, NULL, NULL );
+    clFinish(queue[1]);
+    err = clEnqueueReadBuffer( queue[1] , d_act1, CL_TRUE, 0, sizeof(int) * 128 * 32 * 32, &h_act1, 0, NULL, NULL );
     checkerror(err,"Error: Failed to read kernel arguments! - kernel[1] - d_act1");
 
 
